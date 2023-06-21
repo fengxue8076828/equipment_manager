a4c05391048850c964302250e8b635805a6b805f
York <york@infuture.com>
2023-06-21 15:54:29 +0200

    modify database setting

--- equipment_manager/settings.py
+++ equipment_manager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,14 +105,15 @@ DATABASES = {
     #     "DATABASE_URL",
     #     default="postgres:///equipment_manager",
     # ),
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "postgres",
+    #     "USER": "postgres",
+    #     "PASSWORD": "postgres",
+    #     "HOST": "db",
+    #     "PORT": 5432,
+    # }
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
