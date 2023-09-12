2d7fb9e74a4f269dda3c13b14f24be0dde1547bd
York <york@infuture.com>
2023-09-12 13:47:44 +0200

    __pycache__ folder deleted

--- equipment_manager/settings.py
+++ equipment_manager/settings.py
@@ -168,7 +168,14 @@ AUTH_USER_MODEL = "info_manager.User"
 LOGIN_REDIRECT_URL = "main_panel"
 LOGOUT_REDIRECT_URL = "login"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "lyk20170203@gmail.com"
+EMAIL_HOST_PASSWORD = "TGA121803tga"
+
 
 TEMPLATE_CONTEXT_PROCESSORS = "django.template.context_processors.request"
 
